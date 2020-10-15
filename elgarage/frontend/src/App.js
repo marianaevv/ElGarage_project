a3122b278ebc30cd3ea0403fd1ecb08073070609
Daniel Hernandez <tamdannyhdz@gmail.com>
2020-10-15 17:44:27 -0500

    Prepare admin routes

--- elgarage/frontend/src/App.js
+++ elgarage/frontend/src/App.js
@@ -5,7 +5,9 @@ import Navigation from './components/Navigation';
 import Inicio from './Inicio';
 import Servicios from './Servicios';
 import Citas from './Citas';
+import CitasAdmin from './CitasAdmin';
 import Cotizacion from './Cotizacion';
+import CotizacionAdmin from './CotizacionAdmin';
 import Contacto from './Contacto';
 import Nosotros from './Nosotros';
 import Weather from './weather';
@@ -17,19 +19,22 @@ import './App.css';
 function App() {
   return (
     <Router>
-    <div >
-     <Navigation/>
-     <Switch>
-      <Route path="/" exact component={Inicio}/>
-     <Route path="/Servicios" component={Servicios}/>
-     <Route path="/Citas" component={Citas}/>
-     <Route path="/Cotizacion" component={Cotizacion}/>
-     <Route path="/Contacto" component={Contacto}/>
-     <Route path="/Nosotros" component={Nosotros}/> 
-     <Route path="/Login" component={LoginAdmin}/> 
-     </Switch>
-     
-    </div>
+      <div >
+        <Navigation/>
+        <Switch>
+          <Route path="/" exact component={Inicio}/>
+          <Route path="/Servicios" component={Servicios}/>
+          <Route path="/Citas" component={Citas}/>
+          <Route path="/Cotizacion" component={Cotizacion}/>
+          <Route path="/Contacto" component={Contacto}/>
+          <Route path="/Nosotros" component={Nosotros}/>
+
+          {/* Admin routes */}
+          <Route path="/login" component={LoginAdmin}/>
+          <Route path="/admin/citas" component={CitasAdmin}/>
+          <Route path="/admin/cotizaciones" component={CotizacionAdmin}/>
+        </Switch>
+      </div>
     </Router>
   );
 }
