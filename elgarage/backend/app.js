46df04944a4f123eadf619a1dd262cc5d3272871
Ivan Navarrete <soporte@micronics.mx>
2020-11-01 23:23:55 -0700

    Se agrego la pantalla de nosotros

--- elgarage/backend/app.js
+++ elgarage/backend/app.js
@@ -5,7 +5,6 @@ const mongoose = require("mongoose");
 const morgan = require("morgan");
 const app = express();
 const cors = require( './middleware/cors' );
-require("./db/mongoose");
 const bodyParser = require("body-parser");
 const router = require("./routes/routes");
 app.use(cors);
